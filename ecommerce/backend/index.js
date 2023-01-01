52158faded6b8aba9d9e1d9d62d8377476b94a1a
Aman Raj <78209808+am4n-git@users.noreply.github.com>
2023-01-01 21:57:47 +0530

    rest api to fetch and post product

--- ecommerce/backend/index.js
+++ ecommerce/backend/index.js
@@ -1,47 +1,51 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const { Schema } = require("mongoose");
-
-var mongo = require("mongodb");
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 async function initializeDbConnection() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://aman:admin@neog-try.s7bkzey.mongodb.net/inventory?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log("db connected");
+    await mongoose.connect("mongodb+srv://aman:admin@neog-try.s7bkzey.mongodb.net/inventory?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("db connected")
   } catch (err) {
-    console.log("db connection error", err);
+    console.log("db connection error", err)
   }
 }
 
-initializeDbConnection();
+initializeDbConnection()
 
 // New Schema
 const productSchema = new Schema({
   name: String,
-  price: Number,
-});
+  price: Number
+})
 // New Model
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema)
 
-// Adding a product document to model
-const Watch = new Product({ name: "Rolex", price: 150000 });
-Watch.save()
-  .then((savedWatch) => console.log(savedWatch))
-  .catch((err) => console.log(err));
+app.get('/', (req, res) => {
+  res.json({ msg: "Express server running" })
+});
 
-app.get("/", (req, res) => {});
+// fetch all products from db
+app.get('/product', async (req, res) => {
+    const products = await Product.find({});
+    res.json({ products })
+  });
+
+// add new products
+app.post('/product', (req, res) => {
+  const { name, price } = req.body
+  const Watch = new Product({ name, price })
+  Watch.save().then(savedWatch => res.json(savedWatch)).catch(err => console.log(err))
+})
+
 
 app.listen(3000, () => {
-  console.log("server started");
+  console.log('server started');
 });
-
-module.exports = { initializeDbConnection };
