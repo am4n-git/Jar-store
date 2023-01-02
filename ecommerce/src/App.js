67d70afc0a54d32e13ca68e37a3e4bcc2b5458d1
Aman Raj <78209808+am4n-git@users.noreply.github.com>
2023-01-03 00:07:49 +0530

    card ui for products

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -17,22 +17,26 @@ function App() {
       <Navbar />
       <h1 className="text-3xl">Ecommerce</h1>{" "}
       {products.map((item) => (
-        <div key={item._id}>
-          <Mui.Card>
+        <div key={item._id} className="cardContainer">
+          <Mui.Card className="card">
             <Mui.CardMedia
               component="img"
-              height="10"
-              image="https://picsum.photos/"
+              height="20"
+              width="20"
+              image="https://picsum.photos/20"
               alt="product image alt"
             />
             <Mui.CardContent>
               <Mui.Typography gutterBottom variant="h5" component="div">
                 {item.name}
               </Mui.Typography>
-              <Mui.Typography variant="body2" color="text.secondary">
-                {item.price}
-              </Mui.Typography>
+              <Mui.Typography variant="body2">{item.price}</Mui.Typography>
             </Mui.CardContent>
+            <Mui.CardActions>
+              <Mui.Button size="small" color="primary">
+                Add To Cart
+              </Mui.Button>
+            </Mui.CardActions>
           </Mui.Card>
         </div>
       ))}
