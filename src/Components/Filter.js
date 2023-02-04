5d604d3e07294e4dfe6ccb145eb0da3a587d2792
Aman Raj <78209808+am4n-git@users.noreply.github.com>
2023-02-04 22:58:24 +0530

    moved filter to context

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,32 +1,13 @@
 import React, { useState } from "react";
-import Slider from "@mui/material/Slider";
 import { useProducts } from "../Context/product-data-context";
 function valuetext(value) {
   return `${value}`;
 }
 
 function Filter() {
-  // const { products, setProducts, loading, showError, errorMsg } = useProducts();
-  const [value, setValue] = useState([20, 37]);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  //   setProducts((products) => products.sort((a, b) => b.price - a.price));
-  return (
-    <div>
-      <Slider
-        getAriaLabel={() => "Temperature range"}
-        value={value}
-        onChange={handleChange}
-        min={0}
-        max={50000}
-        step={100}
-        valueLabelDisplay="auto"
-        getAriaValueText={valuetext}
-        sx={{ width: "20rem", pt: 10 }}
-      />
-    </div>
-  );
+  const { products, setProducts } = useProducts();
+
+  return <div></div>;
 }
 
 export default Filter;
---
+++ src/Context/filter-context.js
@@ -0,0 +1,27 @@
+import { createContext, useContext, useReducer } from "react";
+import { useState } from "react";
+import { useProducts } from "../Context/product-data-context";
+
+const FilterContext = createContext({});
+
+const FilterProvider = ({ children, value }) => {
+  const [sortedProduct, setSortedProducts] = useState([]);
+  const { products, setProducts } = useProducts();
+
+  function handleSort(products, action) {
+    switch (action.type) {
+      case "High_To_Low":
+        [...products].sort((a, b) => {
+          b.price - a.price;
+        });
+        break;
+      case "Low_To_High":
+        [...products].sort((a, b) => {
+          return a.price - b.price;
+        });
+        break;
+      default:
+        return;
+    }
+  }
+};
