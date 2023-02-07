ca6e1fe0be1519affe67f9fb6f8dbbda66d108ad
Aman Raj <78209808+am4n-git@users.noreply.github.com>
2023-02-08 00:05:04 +0530

    signup new user

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -3,22 +3,33 @@ import * as Mui from "@mui/material";
 import ArrowForwardIosTwoToneIcon from "@mui/icons-material/ArrowForwardIosTwoTone";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/auth-context";
+import axios from "axios";
 function SignUp() {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [fullName, setFullName] = useState("");
+  // const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const { setIsLoggedIn } = useAuth();
 
-  function showPasswordHandle() {
-    setShowPassword(true);
-  }
+  // function showPasswordHandle() {
+  //   setShowPassword(true);
+  // }
 
   function handleSignUp() {
-    if (password === "1234") {
-      setIsLoggedIn(true);
-      location.state ? navigate(location.state.from.pathname) : navigate("/");
-    }
+    axios
+      .post("http://localhost:3000/user/create", {
+        fullName: fullName,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("server error", error);
+      });
   }
   return (
     <div className="login-container">
@@ -28,12 +39,16 @@ function SignUp() {
           label="Full Name"
           variant="filled"
           className="login-fields"
+          value={fullName}
+          onChange={(event) => setFullName(event.target.value)}
         />
         <Mui.TextField
           id="filled-basic"
           label="Email"
           variant="filled"
           className="login-fields"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
         <Mui.TextField
           id="filled-basic"
@@ -44,7 +59,7 @@ function SignUp() {
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         >
-            <Mui.IconButton></Mui.IconButton>
+          <Mui.IconButton></Mui.IconButton>
         </Mui.TextField>
       </div>
       <div className="login-buttons">
